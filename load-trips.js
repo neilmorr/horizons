1d9c9c4f56831410ed7d4a7ecfb65d6d6309a130
Neil Morrison <16842722+neilmorr@users.noreply.github.com>
2023-10-29 22:42:50 -0600

    Improved success message when trip submitted

--- load-trips.js
+++ load-trips.js
@@ -1,6 +1,6 @@
 import { addDoc, collection, Timestamp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js';
-import { app, db, storage, storageBucket } from '/src/index.js';
 import { ref, uploadBytes, getDownloadURL } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js';
+import { app, db, storage, storageBucket } from '/src/index.js';
 
 // Testing - checking if storage is defined
 if (storage) {
@@ -11,6 +11,26 @@ if (storage) {
 
 console.log(storage); // Testing - checking storage object import successful
 console.log("Storage Bucket Name:", storageBucket); // Testing - checking storage bucket name is correct 
+
+// Function to show the success message popup
+function showPopup(message) {
+    const popup = document.querySelector('.popup-container');
+    const popupMessage = document.querySelector('.popup-message');
+    popupMessage.innerHTML = message; // Set the message content
+
+    popup.style.right = '0';
+
+    // Automatically hide the popup after a delay (e.g., 3 seconds)
+    setTimeout(function() {
+        hidePopup();
+    }, 3000); // 3000 milliseconds = 3 seconds
+}
+
+// Function to hide the success message popup
+function hidePopup() {
+    const popup = document.querySelector('.popup-container');
+    popup.style.right = '-100%'; // Slide it back off-screen to the right
+}
 
 document.getElementById("tripForm").addEventListener("submit", async function (event) {
     event.preventDefault();
@@ -49,10 +69,18 @@ document.getElementById("tripForm").addEventListener("submit", async function (e
             console.log('Trip data added to Firestore:', newTrip);
 
             // Optionally, you can update the calendar here if needed
-            updateCalendar(); // Currently throwing error in console
+            //updateCalendar(); // Currently throwing error in console
 
             // Clear the form fields after successful submission
             document.getElementById("tripForm").reset();
+
+            // Call the showPopup function when you want to display the message
+            showPopup('Trip successfully added!'); // Example usage
+
+            
+
+        
+       
         } catch (error) {
             console.error('Error adding trip data:', error);
         }
